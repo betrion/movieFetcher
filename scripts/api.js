33fa035f256edea1311ebeb530a8b4c931cc927c
dean <forteee@gmail.com>
2022-06-15 15:01:21 +0200

    working version, still needs styling

--- scripts/api.js
+++ scripts/api.js
@@ -12,11 +12,20 @@ async function fetchData(userSearch = "Guardians of the galaxy") {
   return movieData;
 }
 
-const getMovie = async (userSearch, divUpdate) => {
+const tooltipMovie = async (userSearch, divUpdate) => {
   const response = await fetchData(userSearch);
-  console.log(response);
-  globalConfig.search = userSearch;
+  // console.log(response);
+  // globalConfig.search = userSearch;
   divUpdate.innerText = `Did you mean "${response.Title}"?`;
 };
 
-export { getMovie };
+const getMovie = async (userSearch, divUpdate) => {
+  const response = await fetchData(userSearch);
+  // globalConfig.search = userSearch;
+  divUpdate.image.src = `${response.Poster}`;
+  divUpdate.image.alt = `title of movie: ${response.Title} `;
+  divUpdate.title.innerText = `${response.Title}`;
+  divUpdate.description.innerText = `Actors: ${response.Actors}\n Year: ${response.Year}\n Rated: ${response.Rated}\n Director: ${response.Director}\n Runtime: ${response.Runtime} \n\n Plot:\n ${response.Plot}}\n\n Ratings: ${response.Ratings[0].Source} - ${response.Ratings[0].Value}\n\n IMDB link: https://www.imdb.com/title/${response.imdbID} `;
+};
+
+export { tooltipMovie, getMovie };
--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
-import { getMovie } from "./api.js";
+import { getMovie, tooltipMovie } from "./api.js";
 import { globalConfig } from "./config.js";
 
-const movieDisplayDiv = document.querySelector(".movieDiv");
-
+const movieDisplayDiv = {
+  title: document.querySelector(".smallTitle"),
+  image: document.querySelector(".poster"),
+  description: document.querySelector("[data-key='desc'"),
+};
 const currentTooltip = document.querySelector(".currentTitle");
 
 document.querySelector(".search").addEventListener("submit", (e) => {
@@ -11,20 +14,28 @@ document.querySelector(".search").addEventListener("submit", (e) => {
   getMovie(globalConfig.search, currentTooltip);
 });
 
-//logic for clicking and submiting
+//logic for clicking, submiting and live searching
 document.querySelector(".submit").addEventListener("click", (e) => {
   e.preventDefault();
-  getMovie(globalConfig.search, currentTooltip);
+  getMovie(globalConfig.search, movieDisplayDiv);
 });
 
 //update the value in object
 document.querySelector(".search").addEventListener("input", (e) => {
   globalConfig.search = e.target.value;
-  getMovie(globalConfig.search, currentTooltip);
+  tooltipMovie(globalConfig.search, currentTooltip);
 });
 
 //reset button
-document.querySelector(".reset").addEventListener("click", (e) => {
-  movieDisplayDiv.innerHTML = "";
-  currentTooltip.innerHTML = "";
+document.querySelector(".reset").addEventListener("click", () => {
+  resetStuff();
 });
+
+function resetStuff() {
+  movieDisplayDiv.title.innerHTML = "";
+  movieDisplayDiv.image.src = "";
+  movieDisplayDiv.description.innerHTML = "";
+
+  globalConfig.search = "";
+}
+resetStuff();

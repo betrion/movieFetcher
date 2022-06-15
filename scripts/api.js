da181da41c75c27644eb7f5ad23e39bc2030a4fb
betrion <77243490+betrion@users.noreply.github.com>
2022-06-15 17:41:28 +0200

    Secured fetch request
    Wasn't requesting https

--- scripts/api.js
+++ scripts/api.js
@@ -5,7 +5,7 @@ async function fetchData(userSearch = "Guardians of the galaxy") {
   let apikey = globalConfig.apikey;
   let movieTitle = `t=${globalConfig.search}`;
   let movieResponse = await fetch(
-    `http://www.omdbapi.com/?${movieTitle}&${apikey}`,
+    `https://www.omdbapi.com/?${movieTitle}&${apikey}`,
     { headers: { Accept: "application/json" } }
   );
   let movieData = await movieResponse.json();
